037711534f925522d071da8cdfe34bab0cfd8255
mohit <mohitpardeshi729@gmail.com>
2023-04-17 14:06:22 +0530

    added eslint , created server and test folder

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+
 const app = express();
-require('dotenv').config()
+require('dotenv').config();
+
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.listen(PORT , ()=>{
-    console.log(`the port is running on ${PORT}`)
-})
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`the port is running on ${PORT}`);
+});
